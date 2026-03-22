29e87fce894af173f6d26a853eeb5f432a724d59
pablo7410 <pablo23k8@gmail.com>
2026-03-22 19:58:46 +0100

    add drag & drop feature

--- src/app/components/tasks/tasks.ts
+++ src/app/components/tasks/tasks.ts
@@ -3,6 +3,7 @@ import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { FilterType } from '../../app';
 import { AuthService } from '../../service/auth.service';
 import { environment } from '../../../environments/environment.development';
+import { DragDropModule, moveItemInArray, CdkDragDrop } from '@angular/cdk/drag-drop';
 
 interface Task {
   id: number;
@@ -13,7 +14,7 @@ interface Task {
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [DragDropModule],
   standalone: true,
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
@@ -54,18 +55,15 @@ export class TasksComponent implements OnInit {
   addTask(title: string) {
     if (!title.trim()) return;
 
-    // Pamiętaj: authorId: 1 to ID użytkownika, którego stworzyłeś w Prisma Studio
     const newTask = { title, isCompleted: false };
 
     this.http.post(this.apiUrl, newTask).subscribe((savedTask: any) => {
-      // Aktualizujemy sygnał (dodajemy nowe zadanie do listy bez przeładowania strony)
       this.tasks.update((currentTasks) => [...currentTasks, savedTask]);
     });
   }
 
   deleteTask(id: number) {
     this.http.delete(`${this.apiUrl}/${id}`).subscribe(() => {
-      // Usuwamy zadanie z sygnału po udanym DELETE na backendzie
       this.tasks.update((currentTasks) => currentTasks.filter((t) => t.id !== id));
     });
   }
@@ -78,5 +76,17 @@ export class TasksComponent implements OnInit {
           return currentTasks.map((task) => (task.id === updatedTask.id ? updatedTask : task));
         });
       });
+  }
+
+  drop(event: CdkDragDrop<Task[]>) {
+    moveItemInArray(this.tasks(), event.previousIndex, event.currentIndex);
+
+    const reorderedIds = this.tasks().map((t) => t.id);
+
+    this.http.patch(`${this.apiUrl}/reorder`, reorderedIds).subscribe({
+      error: (err) => {
+        console.error('reorder error', err);
+      },
+    });
   }
 }
